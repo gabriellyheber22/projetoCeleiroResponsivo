9a511b240472e4b87dd2ab8d181691abf293a96d
gabriellyheber22 <124007358+gabriellyheber22@users.noreply.github.com>
2023-03-03 22:02:14 -0300

    atualização

---
+++ javascript/menu.js
@@ -0,0 +1,23 @@
+/*Menu em formato de hambuguer para dispositivos de tela pequena*/
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+}
+function clickMenuSuspenso() {
+    if (itensSuspensos.style.display == 'block') {
+        itensSuspensos.style.display = 'none'
+    } else {
+        itensSuspensos.style.display = 'block'
+    }
+
+}
+function clickMenuSuspenso2() {
+    if (itensSuspensos2.style.display == 'block') {
+        itensSuspensos2.style.display = 'none'
+    } else {
+        itensSuspensos2.style.display = 'block'
+    }
+}
