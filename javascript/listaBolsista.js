04aaea2db64acd516c5a0b7ec50323d976f98ba6
gabriellyheber22 <124007358+gabriellyheber22@users.noreply.github.com>
2023-03-04 21:12:36 -0300

    Atualização

---
+++ javascript/listaBolsista.js
@@ -0,0 +1,42 @@
+function clickBolsistaSuspenso() {
+    if (bolsistasSuspensos.style.display == 'block') {
+        bolsistasSuspensos.style.display = 'none'
+    } else {
+        bolsistasSuspensos.style.display = 'block'
+    }
+}
+function clickBolsistaSuspenso01() {
+    if (bolsistasSuspensos01.style.display == 'block') {
+        bolsistasSuspensos01.style.display = 'none'
+    } else {
+        bolsistasSuspensos01.style.display = 'block'
+    }
+}
+function clickBolsistaSuspenso2() {
+    if (bolsistasSuspensos2.style.display == 'block') {
+        bolsistasSuspensos2.style.display = 'none'
+    } else {
+        bolsistasSuspensos2.style.display = 'block'
+    }
+}
+function clickBolsistaSuspenso3() {
+    if (bolsistasSuspensos3.style.display == 'block') {
+        bolsistasSuspensos3.style.display = 'none'
+    } else {
+        bolsistasSuspensos3.style.display = 'block'
+    }
+}
+function clickBolsistaSuspenso4() {
+    if (bolsistasSuspensos4.style.display == 'block') {
+        bolsistasSuspensos4.style.display = 'none'
+    } else {
+        bolsistasSuspensos4.style.display = 'block'
+    }
+}
+function clickBolsistaSuspenso5() {
+    if (bolsistasSuspensos5.style.display == 'block') {
+        bolsistasSuspensos5.style.display = 'none'
+    } else {
+        bolsistasSuspensos5.style.display = 'block'
+    }
+}
---
+++ javascript/responsividade.js
@@ -0,0 +1,7 @@
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {/*Se o tamanho da minha tela for maior ou igual a 768px*/
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
